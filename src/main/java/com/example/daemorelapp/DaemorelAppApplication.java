a150d47cddaf015297bcd24bcffce0d31a500cce
Romi Fadhurrohman Nabil <romi.fadhurrohman@gmail.com>
2022-06-18 13:46:38 +0700

    First commit

---
+++ src/main/java/com/example/daemorelapp/DaemorelAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.daemorelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaemorelAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaemorelAppApplication.class, args);
+    }
+
+}
